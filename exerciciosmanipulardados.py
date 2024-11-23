2bc2f6e169d7bea3c0faa3480629c5f4758f8209
bisawanucleAR <arthurmiranda02011@gmail.com>
2024-11-23 15:06:00 -0400

    manipularDados

---
+++ exerciciosmanipulardados.py
@@ -0,0 +1,6 @@
+n1 = float(input("qual a primeira nota"))
+n2 = float(input("qual a segunda nota"))
+n3 = float(input("qual a terceira nota"))
+
+media = (n1+n2+n3)/3
+print(media)
---
+++ manipularDados.py
@@ -0,0 +1,11 @@
+# nome = "arthur"
+# sobrenome = "miranda*."
+# print(nome + sobrenome)
+
+# print(sobrenome.find("*"))
+# print(len(sobrenome))
+# print(sobrenome.count("."))
+
+email = input("secreva seu emal:")
+print(email)
+print("quantidade decaracters no email: "+len(email))
