2a3402a43ace85833a7a768990d7616b5e3b8475
Arthurmiranda02011 <arthurmiranda02011@gmail.com>
2024-11-30 16:06:38 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,37 @@
+# musicos = ["bruno mars", "trevs scot", "the weeknd"]
+# print(musicos[0])
+# print(musicos[1])
+
+# musicos[1] = "ledy "
+# print(musicos)
+
+# musicos.append("roberto carlos")
+# musicos.insert(3, "luan santana")
+# print(musicos)
+
+# naoGosto = "roberto carlos"
+# musicos.remove(naoGosto)
+# print(musicos)
+
+# musicos.pop()
+# print(musicos)
+
+# musicos.sort()
+# print(musicos)
+
+chamada = ["larissa", "samuel", "diogo", "sayd", "ana luiza"]
+print(chamada[1])
+
+chamada[2] = "paula"
+print(chamada)
+
+
+chamada.append("fernanda")
+print(chamada)
+
+naoGosto = "larissa"
+chamada.remove(naoGosto)
+print(chamada)
+
+chamada.sort()
+print(chamada)
